dd49443df5a0a652979499ff8b01134b1b0d6371
sabrina043 <slachehab6@gmail.com>
2021-03-08 19:51:48 +0100

    fin exo js

--- jour-01/jour01.js
+++ jour-01/jour01.js
@@ -10,18 +10,47 @@ console.log(test);
 
 // exo3
 
+var testLength = "I'm very long";
+console.log(testLength);
+
+
+
+// exo4
+
 var name = "Sabrina"
 console.log("nice to meet you" + name);
 
 
-// exo4
+// exo5
 
 var food = "croissant is meh";
 food = food.replace("meh","so good");
 console.log(food);
 
 
-// exo5
+// exo6
 
 var basic = "this is cool";
-var basicUp = basic.toUpperCase();
+var basicUp = "this is cool";
+var basicDown = "this is cool";
+
+console.log(basic + "" + basicUp.toUpperCase()+ "" +basicDown.toLowerCase());
+
+
+// exo7
+
+var word = "banana";
+var letters = word.split("");
+
+console.log(letters);
+
+
+// exo8
+
+var age = 28;
+var template = `I'm  ${age} years old`;
+
+console.log(template)
+
+
+
