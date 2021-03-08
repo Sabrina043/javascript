67156cdcca38cfb7cec3afce7016c2a7c35da1b2
sabrina043 <slachehab6@gmail.com>
2021-03-08 16:29:39 +0100

    exercice javascript

---
+++ jour01.js
@@ -0,0 +1,27 @@
+// exo1
+
+console.log("hello world !");
+
+// exo2
+
+var test = "My name is Sabrina"
+console.log(test);
+
+
+// exo3
+
+var name = "Sabrina"
+console.log("nice to meet you" + name);
+
+
+// exo4
+
+var food = "croissant is meh";
+food = food.replace("meh","so good");
+console.log(food);
+
+
+// exo5
+
+var basic = "this is cool";
+var basicUp = basic.toUpperCase();
