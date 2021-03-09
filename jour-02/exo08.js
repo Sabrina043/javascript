6cacd088d1363b9c8fd96d677a6c4bdd427c4b14
sabrina043 <slachehab6@gmail.com>
2021-03-09 17:06:58 +0100

    fin des exos js jr2

---
+++ jour-02/exo08.js
@@ -0,0 +1,18 @@
+var limit = 50;
+var score = 45;
+var password = "azerty"
+
+if (score >= limit && "passeword" > "azerty") {  
+
+    console.log("Everything is good");
+}
+      
+   else if (score >= limit | "passeword" > "azerty"){
+    console.log("Something is good");
+       
+}
+ else  {
+
+    console.log("Nothing is good");
+  } 
+     
---
+++ jour-02/exo7.js
@@ -0,0 +1,9 @@
+var password = "azerty"
+
+if ("passeword" > "azerty") {  
+    
+     console.log("the password is secure !");
+      
+  
+       
+  }  
