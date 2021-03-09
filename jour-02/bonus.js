43005578989578962fffb73cf5032d89603945ba
sabrina043 <slachehab6@gmail.com>
2021-03-09 17:32:03 +0100

    Bonus jour2

---
+++ jour-02/bonus.js
@@ -0,0 +1,18 @@
+var random = Math.random();
+
+var min = 1;
+var max = 6;
+var b = Math.floor(random*(max-min+1)+min)
+
+if (b==6) {
+    console.log("yes I win !");
+
+}    
+    
+else   {
+        
+        console.log("so close !");
+    
+    }
+        
+    
