15c444c7a0c838369b257011aa24c232094fb66c
sabrina043 <slachehab6@gmail.com>
2021-03-10 15:31:21 +0100

    Exo3 jr3 js

---
+++ jour-03/exo3.js
@@ -0,0 +1,14 @@
+var objects = ["pen", "book", "lamp"];
+
+
+objects.push("laptop");
+console.log(objects); 
+
+objects.unshift("chair");
+console.log(objects); 
+
+objects.pop("lamp");
+console.log(objects); 
+
+objects.shift("chair");
+console.log(objects); 
