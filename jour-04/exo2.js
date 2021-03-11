1097789bd33a8f46cc188019e1a5222298843618
sabrina043 <slachehab6@gmail.com>
2021-03-11 16:00:58 +0100

    exo2 JS jr 4

---
+++ jour-04/exo2.js
@@ -0,0 +1,18 @@
+var cat2 = {
+    name : "moi",
+    age : 28,
+    isCute : true,
+
+}
+var cat = {
+    name : "garfield",
+    age : 3,
+    isCute : true,
+
+}
+
+var cats = [cat + cat2]
+
+console.table(cat = "age", cat2 = "isCute")
+
+
