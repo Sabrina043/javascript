37d467137fe55a199cca550e45a901bbb75f6208
sabrina043 <slachehab6@gmail.com>
2021-03-11 15:35:40 +0100

    exo1 JS jr 4

---
+++ jour-04/exo1.js
@@ -0,0 +1,18 @@
+var cat = {
+    name : "garfield",
+    age : 3,
+    isCute : true,
+
+   
+  
+}
+
+if (isCute = true) {
+    console.log("So Cute !");
+    
+}
+
+
+
+console.log(cat);
+console.log(cat["age"]);
