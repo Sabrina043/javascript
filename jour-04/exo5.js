958b96ffac7c0b1f8f4bd94a948e212e0f42273b
sabrina043 <slachehab6@gmail.com>
2021-03-12 14:04:56 +0100

    eXo5 Js Jr4

---
+++ jour-04/exo5.js
@@ -0,0 +1,5 @@
+// - Créez une fonction `addUp` qui reçoit un paramètre `num`
+// - À l'aide d'une boucle, ajouter les chiffres de 1 à `num`
+// - Appelez votre fonction avec le chiffre 12 et vérifiez que vous obtenez 78
+
+
