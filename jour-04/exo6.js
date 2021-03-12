6a2646564148c05c9cbcefecf6bd1ebcd013fd79
sabrina043 <slachehab6@gmail.com>
2021-03-12 16:59:29 +0100

    EXO6 js JR4

---
+++ jour-04/exo6.js
@@ -0,0 +1,5 @@
+// - Créez une fonction `format` qui reçoit un paramètre `num` qui représente des secondes
+// - Faites en sorte que la fonction retourne un format `heures : minutes : secondes`
+// - Appelez votre fonction avec l'argument `3700` et vérifiez que vous obtenez `1:1:40`
+
+function format()
