a3d0b2e2fe054b2a464e8fcb833fe9a3ec3f2aaf
sabrina043 <slachehab6@gmail.com>
2021-03-12 13:48:28 +0100

    EXO4 JR4 JS

--- jour-04/exo4.js
+++ jour-04/exo4.js
@@ -1,1 +1,28 @@
-function compare (num1 = 5 * 2 )
+function compare (num1, num2) {
+
+ if  (num1 < num2) {
+     console.log("num2 is bigger")
+ 
+ }
+
+ else if (num1> num2){
+ console.log("num1 is bigger")
+}
+
+else { 
+
+    console.log("both are de same")
+}
+
+
+}
+
+
+compare(40,40)
+
+// - Créez une fonction `compare` qui reçoit deux paramètres `num1` et `num2`
+// - Dans la fonction, à l'aide d'une condition, affichez selon les cas :
+//     - "`num1` is bigger" si `num1` est plus grand
+//     - "`num2` is bigger" si `num2` est plus grand
+//     - "both are the same" si ils sont égaux
+// - Appelez votre fonction plusieurs fois avec des chiffres différents pour vérifier les résultats
