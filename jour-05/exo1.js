8ff40de6675f01b6ad8d48bdeb79461714921d71
sabrina043 <slachehab6@gmail.com>
2021-03-16 17:29:19 +0100

    EXO1 JR5 js

--- jour-05/exo1.js
+++ jour-05/exo1.js
@@ -1,18 +1,33 @@
-function calculate (param1, param2, param3) {
+// CALCULATOR
 
-    if (param3 === "*") { 
-
-        // console.log(param1+param2); 
-
-        console.log(param1*param2);
-        return param1 * param2;
-
-    }
-
-    
-
-    
-
+function calculate (num1, num2, parm) {
+    if (parm === "+") return num1+num2;
+    if (parm === "*") return num1*num2;
+    if (parm === "/") return num1/num2;
+    if (parm === "-") return num1-num2;
 }
-// console.log(calculate(5,4,"*"));
-calculate(5,4,"*");
+
+console.log("la somme est: ", calculate(2,3, "+"));
+console.log("le produit est: ", calculate(5,4, "*"));
+console.log("la division est: ", calculate(2,3, "/"));
+console.log("la soustraction est: ", calculate(2,3, "-"));
+
+
+
+
+// petite révision pour moi
+
+// function calculate (param1, param2, param3) {
+
+//     if (param3 === "*") { 
+
+//         // console.log(param1+param2); 
+
+//         console.log(param1*param2);
+//         return param1 * param2;
+
+//     }  
+
+// }
+// // console.log(calculate(5,4,"*"));
+// calculate(5,4,"*");
