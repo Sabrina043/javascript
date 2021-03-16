afb4ab586c3b9020ce0f32230bd5dc5a190a98ce
sabrina043 <slachehab6@gmail.com>
2021-03-16 09:22:16 +0100

    Révision JS LUNDI

---
+++ revision/revision4.js
@@ -0,0 +1,9 @@
+// - Créez une fonction `swap` qui reçoit une string en paramètre
+// - La fonction retourne une string avec la casse inverse : une majuscule deviendra minuscule et vice-versa
+// - Appelez votre fonction avec l'argument "Hello World" et vérifiez que vous obtenez "hELLO wORLD"
+
+
+function swap(majmin){
+
+    
+}
