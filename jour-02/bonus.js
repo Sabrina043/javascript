add2c5c968fc8af073c7b062f3efe9bd6c9a5beb
sabrina043 <slachehab6@gmail.com>
2021-03-20 20:13:45 +0100

    bonus2 js JR2

--- jour-02/bonus.js
+++ jour-02/bonus.js
@@ -1,3 +1,7 @@
+// - Créez une variable `random` qui contient une valeur aléatoire entre 1 et 6 (nombres entiers seulement)
+// - À l'aide d'une condition, affichez "Yes I win !" si `random` est égal à 6, et "So close..." dans les autres cas
+
+
 var random = Math.random();
 
 var min = 1;
---
+++ jour-02/bonus2.js
@@ -0,0 +1,32 @@
+// - Créez une variable `month` de valeur `"January"`
+// - À l'aide d'un switch, affichez "Winter", "Spring", "Summer" ou "Fall" selon la valeur du mois
+// - Changez la valeur de `month` pour changer le résultat
+
+
+var month = "september";
+
+switch (month){
+
+    case "january":
+        console.log("winter");
+        break;
+
+    case "march":
+        console.log("spring");
+        break;
+
+    case "june":
+        console.log("summer");
+        break;
+
+    case "september":
+         console.log("fall");
+         break;
+
+    default:
+        console.log("la valeur de ta variable ne correspond à aucune de tes réponses.")
+   
+}
+
+
+
