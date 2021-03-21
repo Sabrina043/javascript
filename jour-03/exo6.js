f9d4615b0205bac7de5f316418e61f5a86828642
sabrina043 <slachehab6@gmail.com>
2021-03-22 00:00:47 +0100

    essai exo6 js jr3

--- jour-03/exo6.js
+++ jour-03/exo6.js
@@ -1,5 +1,33 @@
-var sentence = ["Hello", "Konexio", "!"]
+    var sentence = "Hello Konexio !";
+
+    var mysentence = sentence.split("");
+
+    var result = [];
+
+    for (var i = 0; i < mysentence.length; i++) {
+    var item = mysentence[i];
+    
+    
+
+    result.unshift(mysentence);
+}
+
+console.log(result.join(""));
+
+    
+
+      
+ 
+    
+      
+ 
+    // sentence.join("")
+
+
+    //   console.log(sentence)
+
+ 
+ 
 
 
 
-console.log(sentence)
