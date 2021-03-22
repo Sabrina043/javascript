2b804411af349b45e9eac5df5f2970e5fd2ac65b
sabrina043 <slachehab6@gmail.com>
2021-03-22 16:32:49 +0100

    exo4 jour7 js

--- jour-07/exo4.js
+++ jour-07/exo4.js
@@ -5,8 +5,9 @@
 
 var array = [1, "toto", 34, "javascript", 8]
 
-var numbers = array.filter(function(num){
-    return num >= 0
+var numbers = array.filter( function (elem){
+    
+    if (typeof elem === 'number' )
+    {return elem}
 })
-
-console.log(numbers);
+console.log(numbers)
