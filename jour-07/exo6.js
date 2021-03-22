7fb8a429c4a0e1212b860c5bf77b357df302dc85
sabrina043 <slachehab6@gmail.com>
2021-03-22 17:28:33 +0100

    exo6 js jr07

---
+++ jour-07/exo6.js
@@ -0,0 +1,36 @@
+var cakes = [
+    {
+        name: "cake",
+        flavor: "vanilla",
+        status: "available"
+    },
+    {
+        name: "brownie",
+        flavor: "chocolate",
+        status: "available"
+    },
+    {
+        name: "pie",
+        flavor: "strawberry",
+        status: "available"
+    },
+    {
+        name: "muffin",
+        flavor: "pistachio",
+        status: "available"
+    },
+    {
+        name: "donut",
+        flavor: "chocolate",
+        status: "available"
+    },
+]
+
+var cookies = cakes.filter(function (recepice){
+   
+            return recepice.name && recepice.flavor > 20
+        }
+    )
+    
+    console.log(cookies);
+
