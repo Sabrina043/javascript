be3e5691bdbedc13a5cb1241687fc3c9360bdce7
sabrina043 <slachehab6@gmail.com>
2021-03-23 12:15:13 +0100

    exo6 js jr7

--- jour-07/exo6.js
+++ jour-07/exo6.js
@@ -1,36 +1,40 @@
-var cakes = [
-    {
-        name: "cake",
-        flavor: "vanilla",
-        status: "available"
-    },
-    {
-        name: "brownie",
-        flavor: "chocolate",
-        status: "available"
-    },
-    {
-        name: "pie",
-        flavor: "strawberry",
-        status: "available"
-    },
-    {
-        name: "muffin",
-        flavor: "pistachio",
-        status: "available"
-    },
-    {
-        name: "donut",
-        flavor: "chocolate",
-        status: "available"
-    },
-]
-
-var cookies = cakes.filter(function (recepice){
-   
-            return recepice.name && recepice.flavor > 20
+var cakes =
+    [
+        {
+            name: "cake",
+            flavor: "vanilla",
+            status: "available"
+        },
+        {
+            name: "brownie",
+            flavor: "chocolate",
+            status: "available"
+        },
+        {
+            name: "pie",
+            flavor: "strawberry",
+            status: "available"
+        },
+        {
+            name: "muffin",
+            flavor: "pistachio",
+            status: "available"
+        },
+        {
+            name: "donut",
+            flavor: "chocolate",
+            status: "available"
+        },
+    ]
+    var cookie = cakes.map(function (elem){
+        if (elem.flavor === 'chocolate'){
+            elem.status = 'sold out'
         }
-    )
-    
-    console.log(cookies);
+        return elem
+    })
+    console.log(cookie)
+    var cookie2 = cakes.filter(function (elem){
+        return (elem.status === 'sold out')
+    })
+    console.log(cookie2)
 
